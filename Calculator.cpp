6a81dce21bc5261e6dbde0acdb7ab06d7c89556e
IT <IT@3153-16>
2022-05-19 16:40:58 +0900

    completed.

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,5 +1,28 @@
 #include "Calculator.h"
+#include <iostream>
+using namespace std;
 
 double Calculator::Calculate(double x, char oper, double y) {
-
+	if (oper == '+') {
+		return x + y;
+	}
+	else if (oper == '-') {
+		return x - y;
+	}
+	else if (oper == '*') {
+		return x * y;
+	}
+	else if (oper == '/') {
+		if (y == 0.0) {
+			cout << "Error : can't devided by zero." << endl;
+			return 0;
+		}
+		else {
+			return x / y;
+		}
+	}
+	else {
+		cout << "Error : unknown operator. try again." << endl;
+		return 0;
+	}
 }
